664a80b4d6f2e8bc38354a7cd2af649a8c0172aa
hyma999 <hymaaritakula93@gmail.com>
2026-03-11 18:31:38 +0530

    added new

--- infra/00-vpc/main.tf
+++ infra/00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "https://github.com/hyma999/roboshop-infra-dev.gitmodules/terraform-aws-vpc?ref=main"
+    source = "https://github.com/hyma999/roboshop-infra-dev.git//modules/terraform-aws-vpc?ref=main"
 
 project= var.project
 environment= var.environment
