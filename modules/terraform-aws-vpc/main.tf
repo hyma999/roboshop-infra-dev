fbb8c23824a12de544a28f90d706d4f4db3dcbe7
hyma999 <hymaaritakula93@gmail.com>
2026-03-11 19:08:26 +0530

    add

--- modules/terraform-aws-vpc/main.tf
+++ modules/terraform-aws-vpc/main.tf
@@ -152,12 +152,6 @@ resource "aws_nat_gateway" "main" {
  
  }
 
- resource "aws_route" "private" {
-    route_table_id = aws_route_table.private.id
-    destination_cidr_block = "0.0.0.0./0"
-    nat_gateway_id = aws_internet_gateway.main.id
-   
- }
 
 resource "aws_route" "database" {
     route_table_id = aws_route_table.database.id
