498f9f506fbfbaef07095851000adb2487aca7fc
hyma999 <hymaaritakula93@gmail.com>
2026-03-15 21:58:45 +0530

    added

---
+++ 00-vpc/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+    source = "git::https://github.com/hyma999/roboshop-infra-dev.git?ref=main"
+    project= var.project
+    environment= var.environment
+    is_peering_required=true
+}
