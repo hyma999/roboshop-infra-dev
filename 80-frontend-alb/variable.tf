1de04a5c45f6777f99e01318a4853c46c938b7c7
hyma999 <hymaaritakula93@gmail.com>
2026-03-21 16:40:28 +0530

    added

---
+++ 80-frontend-alb/variable.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10031052IOPWYQ5HA3UD"
+}
+
+variable "domain_name" {
+    default = "hymaaws.online"
+}
+
+variable "app_version" {
+    default = "v3"
+}
---
+++ 95-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10031052IOPWYQ5HA3UD"
+}
+
+variable "domain_name" {
+    default = "hymaaws.online"
+}
