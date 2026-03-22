07d6f59e3f89f76038088249f7a462146caa87aa
hyma999 <hymaaritakula93@gmail.com>
2026-03-22 21:48:30 +0530

    add

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "https://github.com/hyma999/roboshop-infra-dev.git?ref=main"
+  source = "git::https://github.com/hyma999/roboshop-infra-dev.git?ref=main"
 
   project              = var.project
   environment          = var.environment
