dc59ddb4c0b66d9a78e4282086fb987a17a42a26
hyma999 <hymaaritakula93@gmail.com>
2026-03-23 13:10:26 +0530

    added

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,6 @@
 module "sg" {
     count = length(var.sg_names)
-    source = "../terraform-aws-sg"
+    source = "git::https://github.com/hyma999/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     sg_names = replace(var.sg_names[count.index],"-","-")
