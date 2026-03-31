174dd7299a50e30bd9707b3dde8c9aa0ee4b6f2c
hyma999 <hymaaritakula93@gmail.com>
2026-03-31 20:13:42 +0530

    add

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,3 +1,4 @@
+
 resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
@@ -32,7 +33,7 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
@@ -71,7 +72,7 @@ resource "terraform_data" "bootstrap_redis" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh redis"
+        "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
 }
@@ -150,7 +151,7 @@ resource "terraform_data" "rabbitmq" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
+        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
     ]
   }
 }
