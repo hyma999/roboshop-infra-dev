1af3ecf63be5faf38456f50abc70dbc3a45fd844
hyma999 <hymaaritakula93@gmail.com>
2026-04-02 13:29:39 +0530

    added

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -1,10 +1,18 @@
-terraform {
-  
+
+ terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "6.33.0" # Terraform AWS provider version
     }
+  }
+
+  backend "s3" {
+    bucket  = "hymaaws" # Replace with your unique bucket name
+    key     = "roboshop-dev-catalogue"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
   }
 }
 
