1cd4abd6dedf493d04342df09353be523dbdb4bb
hyma999 <hymaaritakula93@gmail.com>
2026-04-06 13:19:22 +0530

    added

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -8,7 +8,7 @@
   }
 
   backend "s3" {
-    bucket  = "remote-state-aws-hymaaws-dev" # Replace with your unique bucket name
+    bucket  = "terraform-statefile-hymaawa" # Replace with your unique bucket name
     key     = "roboshop-dev-catalogue"
     region  = "us-east-1"
     encrypt = true
