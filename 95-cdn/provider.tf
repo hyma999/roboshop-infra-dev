d8d03f79ccc5337a6fd6b54dbe2c769d05158ea4
hyma999 <hymaaritakula93@gmail.com>
2026-04-08 13:11:05 +0530

    added

--- 95-cdn/provider.tf
+++ 95-cdn/provider.tf
@@ -1,13 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.33.0"
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
     }
+  }
+
+  backend "s3" {
+    bucket  = "hymaaws" # Replace with your unique bucket name
+    key     = "roboshop-dev-components"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
   }
 }
 
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
