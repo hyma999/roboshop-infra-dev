2f3794b0e104baa0874563d11b0dc5f53820b98e
hyma999 <hymaaritakula93@gmail.com>
2026-04-08 17:01:13 +0530

    added

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -9,4 +9,5 @@ locals {
   user_sg_id = data.aws_ssm_parameter.user_sg_id.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+  openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
 }
